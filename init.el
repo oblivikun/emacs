96f3944190fd8e34e37a051a91dc7e3964c2e32a
oblivikun <dudepog8@gmail.com>
2024-04-19 21:35:01 -0400

    more stuff

---
+++ init.el
@@ -0,0 +1,31 @@
+;; (defvar bootstrap-version)
+;; (let ((bootstrap-file
+;;        (expand-file-name
+;;         "straight/repos/straight.el/bootstrap.el"
+;;         (or (bound-and-true-p straight-base-dir)
+;;             user-emacs-directory)))
+;;       (bootstrap-version 7))
+;; t  (unless (file-exists-p bootstrap-file)
+;;     (with-current-buffer
+;;         (url-retrieve-synchronously
+;;          "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+;;          'silent 'inhibit-cookies)
+;;       (goto-char (point-max))
+;;       (eval-print-last-sexp)))
+
+(org-babel-load-file
+ (expand-file-name "init.org"
+                   "~/oblivimacs/init-org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(reddigg exec-path-from-shell web-beautify undo-tree treemacs-nerd-icons spaceline slime-company projectile pretty-hydra popup org-roam org-mime org-agenda-property org-agenda-files-track-ql org-agenda-files-track lsp-ui lsp-ltex lsp-latex lsp-ivy lsp-haskell lsp-grammarly keycast ivy-hydra indent-guide gcmh elcord dianyou dashboard counsel corfu company-tabnine company-quickhelp company-jedi bug-hunter auctex aggressive-indent)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
